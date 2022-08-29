0a34b5383510b31cf5eca6577ca3b3e61e238602
Dams-d <damien.duro8@gmail.com>
2022-08-29 14:47:44 +0200

    Discover open ID

--- src/wwPlugin.js
+++ src/wwPlugin.js
@@ -2,5 +2,43 @@
 import './components/Configuration/SettingsEdit.vue';
 import './components/Configuration/SettingsSummary.vue';
 /* wwEditor:end */
+import { Issuer } from 'openid-client';
 
-export default {};
+export default {
+    issuer: null,
+    client: null,
+    /*=============================================m_ÔÔ_m=============================================\
+        Plugin API
+    \================================================================================================*/
+    async onLoad(settings) {
+        /* wwFront:start */
+        await this.load(settings.publicData.discoverUrl);
+        /* wwFront:end */
+        /* wwEditor:start */
+        await this.load(settings.publicData.discoverUrl);
+        /* wwEditor:end */
+    },
+    /*=============================================m_ÔÔ_m=============================================\
+        Auth API
+    \================================================================================================*/
+    /*=============================================m_ÔÔ_m=============================================\
+        OpenID API
+    \================================================================================================*/
+    async load(discoverUrl) {
+        try {
+            if (!discoverUrl) return;
+            this.issuer = await Issuer.discover(discoverUrl);
+            if (!this.issuer) throw new Error('Invalid OpenID Auth configuration.');
+            console.log('Discovered issuer %s %O', this.issuer.issuer, this.issuer.metadata);
+            this.client = new this.issuer.Client({});
+            if (!this.client) throw new Error('Invalid OpenID Auth configuration.');
+        } catch (err) {
+            this.issuer = null;
+            this.client = null;
+            wwLib.wwLog.error(err);
+            /* wwEditor:start */
+            wwLib.wwNotification.open({ text: 'Invalid OpenID Auth configuration.', color: 'red' });
+            /* wwEditor:end */
+        }
+    },
+};
