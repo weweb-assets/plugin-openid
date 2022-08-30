96cfd345ef87791f133aa29acd1a29b12ebc0c8f
Dams-d <damien.duro8@gmail.com>
2022-08-30 10:15:37 +0200

    Add actions

--- src/wwPlugin.js
+++ src/wwPlugin.js
@@ -12,11 +12,8 @@ export default {
         Plugin API
     \================================================================================================*/
     async onLoad(settings) {
-        await this.load(
-            settings.publicData.url,
-            settings.publicData.clientId,
-            settings.publicData.afterNotSignInPageId
-        );
+        await this.load(settings.publicData.url, settings.publicData.clientId);
+        await this.fetchUser();
     },
     /*=============================================m_ÔÔ_m=============================================\
         Auth API
@@ -24,20 +21,10 @@ export default {
     /*=============================================m_ÔÔ_m=============================================\
         OpenID API
     \================================================================================================*/
-    async load(url, clientId, afterNotSignInPageId) {
+    async load(url, clientId) {
         try {
-            if (!url || !clientId || !afterNotSignInPageId) return;
-            const websiteId = wwLib.wwWebsiteData.getInfo().id;
-            const redirectTo = wwLib.manager
-                ? `${window.location.origin}/${websiteId}/${afterNotSignInPageId}`
-                : `${window.location.origin}${wwLib.wwPageHelper.getPagePath(afterNotSignInPageId)}`;
-
-            const client = new UserManager({
-                authority: url,
-                client_id: clientId,
-                redirect_uri: redirectTo,
-            });
-            this.client = client;
+            if (!url || !clientId) return;
+            this.client = new UserManager({ authority: url, client_id: clientId });
             if (!this.client) throw new Error('Invalid OpenID Auth configuration.');
         } catch (err) {
             this.client = null;
@@ -48,12 +35,65 @@ export default {
         }
     },
     async fetchUser() {
-        return await this.client.getUser();
+        if (!this.client) throw new Error('Invalid OpenID Auth configuration.');
+
+        try {
+            const user = await this.client.getUser();
+            if (!user) throw new Error('No user authenticated.');
+            wwLib.wwVariable.updateValue(`${this.id}-user`, user);
+            wwLib.wwVariable.updateValue(`${this.id}-isAuthenticated`, true);
+            return user;
+        } catch (err) {
+            wwLib.wwVariable.updateValue(`${this.id}-user`, null);
+            wwLib.wwVariable.updateValue(`${this.id}-isAuthenticated`, false);
+            throw err;
+        }
     },
-    async login() {
-        return await this.client.signinRedirect();
+    async loginWithPopup() {
+        if (!this.client) throw new Error('Invalid OpenID Auth configuration.');
+
+        const websiteId = wwLib.wwWebsiteData.getInfo().id;
+        const redirectTo = wwLib.manager
+            ? `${window.location.origin}/${websiteId}/${this.settings.publicData.afterSignInPageId}`
+            : `${window.location.origin}${wwLib.wwPageHelper.getPagePath(this.settings.publicData.afterSignInPageId)}`;
+
+        await this.client.signinPopup({ redirect_uri: redirectTo });
+        return await this.fetchUser();
     },
-    async logout() {
-        return await this.client.signoutRedirect();
+    async loginWithRedirect() {
+        if (!this.client) throw new Error('Invalid OpenID Auth configuration.');
+
+        const websiteId = wwLib.wwWebsiteData.getInfo().id;
+        const redirectTo = wwLib.manager
+            ? `${window.location.origin}/${websiteId}/${this.settings.publicData.afterSignInPageId}`
+            : `${window.location.origin}${wwLib.wwPageHelper.getPagePath(this.settings.publicData.afterSignInPageId)}`;
+
+        return await this.client.signinRedirect({ redirect_uri: redirectTo });
+    },
+    async logoutWithPopup() {
+        if (!this.client) throw new Error('Invalid OpenID Auth configuration.');
+
+        const websiteId = wwLib.wwWebsiteData.getInfo().id;
+        const redirectTo = wwLib.manager
+            ? `${window.location.origin}/${websiteId}/${this.settings.publicData.afterNotSignInPageId}`
+            : `${window.location.origin}${wwLib.wwPageHelper.getPagePath(
+                  this.settings.publicData.afterNotSignInPageId
+              )}`;
+
+        await this.client.signoutPopup({ redirect_uri: redirectTo });
+        wwLib.wwVariable.updateValue(`${this.id}-user`, null);
+        wwLib.wwVariable.updateValue(`${this.id}-isAuthenticated`, false);
+    },
+    async logoutWithRedirect() {
+        if (!this.client) throw new Error('Invalid OpenID Auth configuration.');
+
+        const websiteId = wwLib.wwWebsiteData.getInfo().id;
+        const redirectTo = wwLib.manager
+            ? `${window.location.origin}/${websiteId}/${this.settings.publicData.afterNotSignInPageId}`
+            : `${window.location.origin}${wwLib.wwPageHelper.getPagePath(
+                  this.settings.publicData.afterNotSignInPageId
+              )}`;
+
+        return await this.client.signoutRedirect({ redirect_uri: redirectTo });
     },
 };
--- ww-config.js
+++ ww-config.js
@@ -16,10 +16,41 @@ export default {
                 edit: () => import('./src/components/Redirections/SettingsEdit.vue'),
                 summary: () => import('./src/components/Redirections/SettingsSummary.vue'),
                 getIsValid(settings) {
-                    const { afterNotSignInPageId } = settings.publicData;
-                    return !!afterNotSignInPageId;
+                    const { afterSignInPageId, afterNotSignInPageId } = settings.publicData;
+                    return !!afterSignInPageId && !!afterNotSignInPageId;
                 },
             },
         ],
     },
+    variables: [
+        { name: 'user', value: 'user', type: 'object', defaultValue: null },
+        { name: 'isAuthenticated', value: 'isAuthenticated', type: 'boolean', defaultValue: false },
+    ],
+    actions: [
+        {
+            name: 'Login with Popup',
+            code: 'loginWithPopup',
+            isAsync: true,
+        },
+        {
+            name: 'Login with Redirect',
+            code: 'loginWithPopup',
+            isAsync: true,
+        },
+        {
+            name: 'Logout with Popup',
+            code: 'logoutWithPopup',
+            isAsync: true,
+        },
+        {
+            name: 'Logout with Redirect',
+            code: 'logoutWithRedirect',
+            isAsync: true,
+        },
+        {
+            name: 'Fetch User',
+            code: 'fetchUser',
+            isAsync: true,
+        },
+    ],
 };
